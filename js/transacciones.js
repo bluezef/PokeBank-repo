343c6e7436fe89ffc3bffd45b2dcd3725db29722
Ernesto Gonzalez <neto.gp3@gmail.com>
2024-05-13 23:16:28 -0600

    Added aesthetic criteria for displaying transaction list

--- js/transacciones.js
+++ js/transacciones.js
@@ -10,10 +10,34 @@ function returnList(){
 }
 
 function showList(list){
-    const listElement=document.getElementById('transacciones');
+    const listElement=document.getElementById('transaccionesul');
+    let title = document.createElement('li');
+    title.textContent='Descripción'
+    listElement.appendChild(title);
     for(let i=0;i<list.length;i+=2){
         const li = document.createElement('li');
-        li.textContent=list[i]+' $'+list[i+1];
+        li.textContent=list[i]
         listElement.appendChild(li);
     }
+    const listElement2=document.getElementById('montosul');
+    let title2 = document.createElement('li');
+    title2.textContent='Monto'
+    listElement2.appendChild(title2);
+    for(let i=1;i<list.length;i+=2){
+        const li = document.createElement('li');
+        li.textContent='$'+list[i];
+        listElement2.appendChild(li);
+    }
 }
+
+const range = (start, end, step = 1) => {
+    let output = [];
+    if (typeof end === 'undefined') {
+      end = start;
+      start = 0;
+    }
+    for (let i = start; i < end; i += step) {
+      output.push(' ');
+    }
+    return output;
+};
